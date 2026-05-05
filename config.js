f0af26f6561f037f673982fb8c4e6aacca321dc3
Sai Maanas <saimaanas2719@gmail.com>
2026-05-05 09:45:48 +0530

    Update IMGBB API key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  IMGBB_API_KEY: "b9f93f2cbb186f5f64d20ac4e545743b",
+  IMGBB_API_KEY: "fda49052a6f727c1e4ce3266a153f7cb",
   N8N_WEBHOOK: "http://localhost:5678/webhook-test/ewaste-camera"
 };
